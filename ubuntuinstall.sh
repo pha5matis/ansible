45089f82b10ca5e34b5ce196522d64d4ad2e3e90
root <root@online.phasmatis.net>
2017-01-10 09:11:48 +0000

    various fixes

--- ubuntuinstall.sh
+++ ubuntuinstall.sh
@@ -3,3 +3,8 @@ apt-get install software-properties-common
 apt-add-repository ppa:ansible/ansible
 apt-get update
 apt-get install ansible -y
+ssh-keygen
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+sed -i -e 's/#AuthorizedKeysFile/AuthorizedKeysFile/' /etc/ssh/sshd_config
+service ssh restart
